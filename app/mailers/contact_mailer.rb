e6d65d4b464e6c8c80c893efe25d8972f0e5b5f5
Paul Petherick <petherickp@gmail.com>
2015-08-07 17:57:32 +0000

    corrected email method

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
     default to: 'petherickp@gmail.com'
     
-    def contact_emial(name, email, body)
+    def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
